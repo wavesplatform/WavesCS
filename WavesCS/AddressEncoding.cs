e40e768a8469bf8f1d86b099e65b3a8d210b02c3
abegicheva <abegicheva@wavesplatform.com>
2017-12-28 13:43:15 +0300

    add some other files

---
+++ WavesCS/AddressEncoding.cs
@@ -0,0 +1,42 @@
+﻿using HashLib;
+using System.IO;
+
+namespace WavesCS
+{
+    public class AddressEncoding
+    {
+        public static char MainNet = 'W';
+        public static char TestNet = 'T';
+
+        private static readonly IHash Keccak256 = HashFactory.Crypto.SHA3.CreateKeccak256();
+
+        protected static byte[] Hash(byte[] message, int offset, int lenght, IHash algorithm)
+        {
+            algorithm.Initialize();
+            algorithm.TransformBytes(message, offset, lenght);
+            HashResult result = algorithm.TransformFinal();
+            return result.GetBytes();
+        }
+
+        protected static byte[] SecureHash(byte[] message, int offset, int lenght)
+        {
+            Blake2Sharp.Blake2BConfig config = new Blake2Sharp.Blake2BConfig();
+            config.OutputSizeInBits = 256;
+            byte[] blake2b = Blake2Sharp.Blake2B.ComputeHash(message, offset, lenght, config);
+            return Hash(blake2b, 0, blake2b.Length, Keccak256);
+        }
+
+        protected static string GetAddressFromPublicKey(byte[] publicKey, char scheme)
+        {
+            MemoryStream stream = new MemoryStream(26);
+            byte[] hash = SecureHash(publicKey, 0, publicKey.Length);
+            BinaryWriter writer = new BinaryWriter(stream);
+            writer.Write((byte)1);
+            writer.Write((byte)scheme);
+            writer.Write(hash, 0, 20);
+            byte[] checksum = SecureHash(stream.ToArray(), 0, 22);
+            writer.Write(checksum, 0, 4);
+            return Base58.Encode(stream.ToArray());
+        }        
+    }
+}
