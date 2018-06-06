a7b240a673218c6477fa01163ea4b6e7599b971d
Alexey Kofman <al.kofman@gmail.com>
2018-06-06 20:34:49 +0300

    Base58 fix

---
+++ WavesCSTests/Base58Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WavesCS;
+
+namespace WavesCSTests
+{
+    [TestClass]
+    public class Base58Test
+    {
+        
+        [TestMethod]
+        public void TestEncodeDecode()
+        {
+            var address = "3N1JMgUfzYUZinPrzPWeRa6yqN67oo57XR7";
+
+            Assert.AreEqual(address, address.FromBase58().ToBase58());
+                       
+        }
+    }
+}
