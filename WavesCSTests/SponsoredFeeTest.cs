e3e79020de1c134cd32a2f435b2820ccf7192dc5
Antonina <be-ton@yandex.ru>
2018-06-27 16:00:57 +0300

    fix sponsoredFeeTx generation and test, add Node method for SponsoredFeeTx and test for it

--- WavesCSTests/SponsoredFeeTest.cs
+++ WavesCSTests/SponsoredFeeTest.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using WavesCS;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -19,23 +20,21 @@ namespace WavesCSTests
         [TestMethod]
         public void TestSponsoredFeeTransaction()
         {
-            var node = new Node();     
-            //var issueTx = new IssueTransaction(Accounts.Alice.PublicKey, "testAsset3", "asset for c# issue testing", 100, 8, false);
-         
+            var node = new Node();
+            var issueTx = new IssueTransaction(Accounts.Alice.PublicKey, "testAsset", "asset for c# issue testing", 2, 6, false);
+            issueTx.Sign(Accounts.Alice);
+            string response = node.Broadcast(issueTx.GetJsonWithSignature()); 
+            Assert.IsFalse(string.IsNullOrEmpty(response));
 
-            //issueTx.Sign(Accounts.Alice);
-            //var jsonobj = issueTx.GetJson().ToString();
-            //string response = node.Broadcast(issueTx.GetJsonWithSignature());
-            //Console.WriteLine(response);
-            //Assert.IsFalse(string.IsNullOrEmpty(response));
+            Thread.Sleep(10000);
 
-            //var txId = response.ParseJsonObject().GetString("id");
-
-            Asset asset = Assets.GetById("3TUubhCKKxwTKgYRM4SrwPGYafyAp4Z4ZK98wyY4XWCt", node);
-            Decimal minimalFeeInAssets = 100000;
+            string assetId = response.ParseJsonObject().GetString("assetId");
+            Asset asset = Assets.GetById(assetId, node);
+            var minimalFeeInAssets = 0.0001m;
             var sponsoredTx = new SponsoredFeeTransaction(Accounts.Alice.PublicKey, asset, minimalFeeInAssets);
             sponsoredTx.Sign(Accounts.Alice);
             var responseTwo = node.Broadcast(sponsoredTx.GetJsonWithSignature());
+            Assert.IsFalse(string.IsNullOrEmpty(responseTwo));
 
         }
     }
