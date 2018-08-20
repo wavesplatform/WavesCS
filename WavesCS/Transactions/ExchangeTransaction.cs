36ad5f3fd68b8bfc02fe095e7586fc846171b531
Dmitry Kiselev <dkiselev@wavesplatform.com>
2018-08-20 18:13:34 +0300

    fixed ExchangeTransaction

--- WavesCS/Transactions/ExchangeTransaction.cs
+++ WavesCS/Transactions/ExchangeTransaction.cs
@@ -39,7 +39,7 @@ namespace WavesCS
 
         public override byte[] GetBody()
         {
-            throw new Exception("???");
+            throw new Exception();
         }
 
         public override Dictionary<string, object> GetJson()
