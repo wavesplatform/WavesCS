782a7b8293813dd55f4a0c1c1f232064ec9bc68a
Dmitry Kiselev <dkiselev@wavesplatform.com>
2018-08-20 18:05:25 +0300

    changed indents

--- WavesCS/Transactions/ExchangeTransaction.cs
+++ WavesCS/Transactions/ExchangeTransaction.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using DictionaryObject = System.Collections.Generic.Dictionary<string, object>;
-
 
 namespace WavesCS
 {
