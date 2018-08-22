3645df038a97997e928c36e5404f16c84a863b61
Dmitry Kiselev <dkiselev@wavesplatform.com>
2018-08-22 11:58:33 +0300

    add ExchangeTransaction test

---
+++ WavesCSTests/ExchangeTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using WavesCS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WavesCSTests
+{
+    [TestClass]
+    public class ExchangeTest
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Http.Tracing = true;
+        }
+
+        [TestMethod]
+        public void TestExchangeTransaction()
+        {
+            var node = new Node();
+
+
+        }
+    }
+}
