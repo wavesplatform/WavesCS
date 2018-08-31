f845beb476636ab1315e097fdde6b468c62ca752
Antonina <abegicheva@wavesplatform.com>
2018-08-31 13:39:22 +0300

    make refactoring and fix versions

--- WavesCSTests/DeserializeTransactionsTest.cs
+++ WavesCSTests/DeserializeTransactionsTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+﻿using System.Linq;
 using WavesCS;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Text;
 
 namespace WavesCSTests
 {
