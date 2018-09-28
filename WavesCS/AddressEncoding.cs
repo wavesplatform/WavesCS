a236a0d27db9cfe1a4a3cfdb8bb3eff4a3d0e06e
Dmitry Kiselev <dkiselev@wavesplatform.com>
2018-09-28 21:24:29 +0300

    delete commented info

--- WavesCS/AddressEncoding.cs
+++ WavesCS/AddressEncoding.cs
@@ -7,7 +7,7 @@ namespace WavesCS
     public static class AddressEncoding
     {
         public static char MainNet = 'W';
-        public static char TestNet = 'T'; // 'S'
+        public static char TestNet = 'T';
 
         private static readonly IHash Keccak256 = HashFactory.Crypto.SHA3.CreateKeccak256();
 
