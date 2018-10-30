5ec7a0fb2b6491c6e93c5d7c07ab6ddf66762c90
Dmitry Kiselev <dkiselev@wavesplatform.com>
2018-10-30 12:45:31 +0300

    add SetScriptTransaction

---
+++ WavesCS/Transactions/SetAssetScriptTransaction.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace WavesCS
+{
+    public class SetAssetScriptTransaction : Transaction
+    {
+        public Asset Asset;
+        public byte[] Script { get; }
+        public char ChainId { get; }
+
+        public override byte Version { get; set; } = 1;
+
+        public SetAssetScriptTransaction(byte[] senderPublicKey, Asset asset, byte[] script, char chainId, decimal fee = 1m) : base(senderPublicKey)
+        {
+            Asset = asset;
+            Script = script;
+            ChainId = chainId;
+            Fee = fee;
+        }
+
+        public SetAssetScriptTransaction(Dictionary<string, object> tx) : base(tx)
+        {
+            Script = tx.GetString("script").FromBase64();
+
+            var node = new Node(ChainId);
+            Asset = node.GetAsset(tx.GetString("assetId"));
+            Fee = Assets.WAVES.LongToAmount(tx.GetLong("fee"));
+            ChainId = 'W';
+        }
+
+        public override byte[] GetBody()
+        {
+            using (var stream = new MemoryStream())
+            using (var writer = new BinaryWriter(stream))
+            {
+                writer.Write(TransactionType.SetAssetScript);
+                writer.Write(Version);
+                writer.Write(ChainId);
+                writer.Write(SenderPublicKey);
+
+                writer.Write(Asset.Id.FromBase58());
+
+                writer.WriteLong(Assets.WAVES.AmountToLong(Fee));
+                writer.WriteLong(Timestamp.ToLong());
+
+                if (Script == null)
+                {
+                    writer.Write((byte)0);
+                }
+                else
+                {
+                    writer.Write((byte)1);
+                    writer.WriteShort((short)Script.Length);
+                    writer.Write(Script);
+                }
+
+                return stream.ToArray();
+            }
+        }
+
+        public override byte[] GetIdBytes()
+        {
+            return GetBody();
+        }
+
+        public override Dictionary<string, object> GetJson()
+        {
+            return new Dictionary<string, object>
+            {
+                {"type", TransactionType.SetAssetScript},
+                {"assetId", Asset.Id},
+                {"version", Version},
+                {"senderPublicKey", SenderPublicKey.ToBase58()},
+                {"script", Script?.ToBase64()},
+                {"fee", Assets.WAVES.AmountToLong(Fee)},
+                {"timestamp", Timestamp.ToLong()}
+            };
+        }
+
+        protected override bool SupportsProofs()
+        {
+            return true;
+        }
+    }
+}
---
+++ WavesCSTests/SmartAssetsTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WavesCS;
+
+namespace WavesCSTests
+{
+    [TestClass]
+    public class SmartAssetsTest
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Http.Tracing = true;
+        }
+
+        [TestMethod]
+        public void TestIssueSmartAsset()
+        {
+            var node = new Node();
+
+            var script = "true";
+            var compiledScript = node.CompileScript(script);
+
+
+            Asset asset = node.IssueAsset(Accounts.Alice, "testSmartAsset",
+                                          "Smart Asset for c# testing", 2, 6,
+                                          true, compiledScript);
+            Assert.IsNotNull(asset);
+
+            Thread.Sleep(15000);
+
+            var quantity = node.GetBalance(Accounts.Alice.Address, asset);
+            Assert.AreEqual(quantity, 2);
+        }
+
+
+        [TestMethod]
+        public void TestSetAssetScript()
+        {
+            var node = new Node();
+
+        }
+    }
+}
