e1813709f9311a3d126a8b9e14d871fc873909be
Dmitry Kiselev <dkiselev@wavesplatform.com>
2018-10-31 18:36:40 +0300

    change TestSetAssetScript

--- WavesCSTests/SmartAssetsTest.cs
+++ WavesCSTests/SmartAssetsTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WavesCS;
 
@@ -7,6 +8,8 @@ namespace WavesCSTests
     [TestClass]
     public class SmartAssetsTest
     {
+        Asset smartAsset = new Asset("", "", 8);
+
         [TestInitialize]
         public void Init()
         {
@@ -22,21 +25,66 @@ namespace WavesCSTests
             var compiledScript = node.CompileScript(script);
 
 
-            Asset asset = node.IssueAsset(Accounts.Alice, "testSmartAsset",
-                                          "Smart Asset for c# testing", 2, 6,
+            smartAsset = node.IssueAsset(Accounts.Alice, "SmartAsset",
+                                          "Smart Asset", 100, 4,
                                           true, compiledScript);
-            Assert.IsNotNull(asset);
+            Assert.IsNotNull(smartAsset);
 
             Thread.Sleep(15000);
 
-            var quantity = node.GetBalance(Accounts.Alice.Address, asset);
-            Assert.AreEqual(quantity, 2);
+            var quantity = node.GetBalance(Accounts.Alice.Address, smartAsset);
+            Assert.AreEqual(quantity, 100);
         }
 
         [TestMethod]
         public void TestSetAssetScript()
         {
             var node = new Node();
+
+            var script = $@"                
+                match tx {{
+                    case tx : TransferTransaction => tx.amount < 300000
+                    case tx : BurnTransaction | SetAssetScriptTransaction => true
+                    case _  => false
+                }}";
+
+            var asset = Assets.GetById("");
+
+            var compiledScript = node.CompileScript(script);
+
+            var setAssetScriptTransaction = new SetAssetScriptTransaction(Accounts.Alice.PublicKey,
+                                                                          asset, compiledScript,
+                                                                          'T', 1);
+
+            setAssetScriptTransaction.Sign(Accounts.Alice);
+            node.Broadcast(setAssetScriptTransaction);
+
+            Thread.Sleep(4000);
+
+            var balanceBefore = ;
+
+            for (decimal amount = 0.01m; amount < 0.5m; amount += 0.1m)
+            {
+                try
+                {
+                    node.Transfer(Accounts.Alice, Accounts.Bob.Address, smartAsset, amount);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+
+            Thread.Sleep(5000);
+
+            var balanceAfter = ;
+
+            Assert.AreEqual(balanceAfter, balanceBefore + 0.01m + 0.11m + 0.21m);
+
+            // + check the fee
+
+
         }
     }
 }
