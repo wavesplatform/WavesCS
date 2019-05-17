8b2b88275ac7aae9312a0a337fe23278ca5beb90
Antonina <abegicheva@wavesplatform.com>
2019-05-17 19:58:38 +0300

    fix test

--- WavesCSTests/ProtobufTest.cs
+++ WavesCSTests/ProtobufTest.cs
@@ -24,20 +24,9 @@ namespace WavesCSTests.Protobuf
         public async Task TestGrpc()
         {
             Channel channel = new Channel("mainnet-aws-fr-3.wavesnodes.com:6870", ChannelCredentials.Insecure);
-            //TransactionsApi.TransactionsApiClient client = new TransactionsApi.TransactionsApiClient(channel);
             BlocksApi.BlocksApiClient client = new BlocksApi.BlocksApiClient(channel);
             BlockWithHeight block = client.GetBlock(new BlockRequest() { Height = -1 });
-            TestContext.WriteLine(block.ToString());
-            // {
-            //    Recipient = new Recipient() { Address = Google.Protobuf.ByteString.CopyFrom(Base58.Decode("3N1atv1SuhTC3sQwqrR6BkK2PNwrC8LKLqp")) },
-            //    Sender = Google.Protobuf.ByteString.CopyFrom(Base58.Decode("3N8S8VUbDJuqtUP8wAraWhMMeqcYHH5EWcF"))
-            //}).ResponseStream;
-            //int i = 0;
-            //while (await txs.MoveNext() && i < 10)
-            //{
-            //    TestContext.WriteLine(txs.Current.ToString());
-            //    i++;
-            //}
+            TestContext.WriteLine(block.ToString());           
         }
     }
 }
