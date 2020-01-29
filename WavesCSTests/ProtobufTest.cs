cd6848cca7c7f285b69c249b14d4d59686ede589
d-kiselev <dkiselev1234@gmail.com>
2020-01-29 13:42:41 +0300

    add TestGrpcTransactionsApiClient

--- WavesCSTests/ProtobufTest.cs
+++ WavesCSTests/ProtobufTest.cs
@@ -3,6 +3,8 @@ using Grpc.Core;
 using Waves;
 using Waves.Node.Grpc;
 using WavesCS;
+using Google.Protobuf;
+using System.Threading.Tasks;
 
 namespace WavesCSTests.Protobuf
 {
@@ -18,12 +20,35 @@ namespace WavesCSTests.Protobuf
         }
 
         [TestMethod]
-        public void TestGrpc()
+        public void TestGrpcBlocksApiClient()
         {
             Channel channel = new Channel("mainnet-aws-fr-1.wavesnodes.com:6871", ChannelCredentials.Insecure);
             BlocksApi.BlocksApiClient client = new BlocksApi.BlocksApiClient(channel);
             BlockWithHeight block = client.GetBlock(new BlockRequest() { Height = -1 });
             TestContext.WriteLine(block.ToString());
         }
+
+        [TestMethod]
+        public void TestGrpcTransactionsApiClient()
+        {
+            Channel channel = new Channel("mainnet-aws-fr-1.wavesnodes.com:6871", ChannelCredentials.Insecure);
+            
+            var client = new TransactionsApi.TransactionsApiClient(channel);
+
+            var txId = "76TxtthzU6YxLMAEVpPj7RJtJcXAG8FCRvDZy4KR4zW2";
+            var request = new TransactionsRequest()
+            {
+                TransactionIds = { ByteString.CopyFrom(txId.FromBase58()) }
+            };
+
+            var t = client.GetTransactions(request);
+            var task = Task.Run(async () => { await t.ResponseStream.MoveNext(); });
+            task.Wait();
+
+            TestContext.WriteLine(t.ResponseStream.Current.ToString());
+
+        }
+
+
     }
 }
